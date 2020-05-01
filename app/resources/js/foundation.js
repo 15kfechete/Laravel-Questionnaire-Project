20f3d513b518d4356dfde7b7c4f2a5a2969b9cf8
23932872 <23932872@edgehill.ac.uk>
2020-05-01 16:20:39 +0100

    Installed Foundation Functionality

--- app/resources/js/foundation.js
+++ app/resources/js/foundation.js
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+window._ = require("lodash");
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -6,9 +6,15 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+try {
+    window.$ = window.jQuery = require("jquery");
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    require("foundation-sites");
+} catch (e) {}
+
+window.axios = require("axios");
+
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
@@ -26,3 +32,5 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
+
+$(document).foundation(); // initialize foundation
