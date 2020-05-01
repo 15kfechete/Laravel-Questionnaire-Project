4a0dfe642b4fb34d604e21b2c87c751d21d89d90
23932872 <23932872@edgehill.ac.uk>
2020-05-01 17:20:14 +0100

    Created Initial Migrations

---
+++ app/database/migrations/2020_05_01_153510_create_questionnaires_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionnairesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questionnaires', function (Blueprint $table) {
+            $table->bigIncrements('questionnaireID');
+            $table->unsignedBigInteger('researcherID');
+            $table->string('questionnaireID');
+            $table->date_create('createdOn');
+            $table->date_date_set('deadlineOn');
+            $table->string('agreementTerms');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questionnaires');
+    }
+}
